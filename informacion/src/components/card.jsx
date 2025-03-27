debe4127e4575636cded6bc492615e5ba07bcb7a
unknown <fernand0dejesus>
2025-03-27 10:29:31 -0600

    segundo commit

--- informacion/src/components/card.jsx
+++ informacion/src/components/card.jsx
@@ -3,12 +3,18 @@ import './Test.jsx'
 
 const Card =({contenedor})=>{
     return(
-        
-<div class="card">
-  <div class="card-body">
-    This is some text within a card body.
-  </div>
-</div>
+        <div class="card" >
+        <img src="https://betterguards.com/cdn/shop/articles/volleyball-verletzungen-praevention.png?v=1677139337&width=2048" class="card-img-top" alt="..."/>
+        <div class="card-body">
+          <h1 class="card-title">Fernando de Jesus </h1>
+          <p class="card-text">Solia entrenar Volleyball en el colegio y en los entrenos en el 2023 y 2024</p>
+          <p>estudio en:Instituto Técnico Ricaldone
+voy a: Tercer año de bachillerato
+Desarrollo de Software
+</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
     )
 }
 
