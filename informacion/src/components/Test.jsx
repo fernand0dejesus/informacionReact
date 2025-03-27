17a7b8ba61b4a4fa4ada8df69ad3489f7b9565c4
unknown <fernand0dejesus>
2025-03-27 09:55:23 -0600

    primer commit

---
+++ informacion/src/components/Test.jsx
@@ -0,0 +1,32 @@
+import './Test.jsx' 
+
+
+const Test = () => {
+
+/*const estilos={
+    contenedor:{
+        textAlign:'center',
+        backgroundColor: 'red',
+        padding: '20px',
+        borderRadius:'10px'
+    },
+    titulo:{
+        color:'#333',
+        fontSize: '24px'
+
+    },
+    parrafo:{
+
+         color:'#666',
+        fontSize: '16px'
+    }
+    }
+*/
+   return ( 
+    <div className='contenedor'>
+        <h1 className='titulo'>Test</h1>
+        <p>Este es un componente de prueba</p>
+    </div>
+ );
+}
+export default Test; 
---
+++ informacion/src/components/card.jsx
@@ -0,0 +1,16 @@
+import './Test.jsx' 
+
+
+const Card =({contenedor})=>{
+    return(
+        
+<div class="card">
+  <div class="card-body">
+    This is some text within a card body.
+  </div>
+</div>
+    )
+}
+
+
+export default Card;
